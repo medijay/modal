6eaf62991522f1b310eb117264877cf1210e4ba7
Ahmed Adekunle <medijay99@yahoo.com>
2023-03-07 18:20:32 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+
+const modalBtn = document.querySelector(".modal-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener("click",function(){
+     //console.log(moderove.classList);
+     if (modalOverlay.classList.contains('open-modal')) {
+modalOverlay.classList.remove("open-modal");
+     } else {
+        modalOverlay.classList.add("open-modal");
+     }
+});
+
+closeBtn.addEventListener("click", function(){
+ (modalOverlay.classList.remove('open-modal'));
+});
